28ba87fd3a842ed563de60da19106f39e3569693
Alessio Di Lorenzo <a.dilorenzo@izs.intra>
2023-11-23 18:25:13 +0100

    Aggiunta possibilita multiselect regione e customizzazione gruppi di tiles

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,12 @@ if 'to_date' not in st.session_state:
 
 if 'day_delta' not in st.session_state:
     st.session_state['day_delta'] = ''
+    
+if 'selected_regions' not in st.session_state:
+    st.session_state['selected_regions'] = ['Abruzzo']
+    
+if 'region_tiles' not in st.session_state:
+    st.session_state['region_tiles'] = ['R122_T33TUG', 'R122_T33TUH', 'R079_T33TVG', 'R122_T33TVH']
 
 st.header(':satellite: Ricerca immagini Sentinel 2', divider='blue')
 st.write('*Copernicus Data Space Ecosystem OData API*')
@@ -30,31 +36,33 @@ st.write('*Copernicus Data Space Ecosystem OData API*')
 # Launch request
 df_list = []
 def call_ecosystem_api():
-    
-    # Check dates
-    print(type(st.session_state['from_date']))
-    delta = st.session_state['to_date'] - st.session_state['from_date']
-    # print(delta.days)
-    if delta.days >= 0:
-        st.session_state['day_delta'] = 'Valid'
-        for req in request_list:
-            # print(req)
-            json = requests.get(req).json()
-            # print(json)
-            # DataFrame drlla risposta
-            if (len(json['value'])>0):
-                df = pd.DataFrame.from_dict(json['value']).dropna()
-                df = df[['Id', 'Name','OriginDate','GeoFootprint','Online']]
-                # print('Immagini trovate per la singola richiesta: {}'.format(str(len(df))))
-                df_list.append(df)
-            else:
-                pass
-    
-        df_tot = pd.concat(df_list, ignore_index=True)
-        df_tot.sort_values(by='OriginDate', ascending = False, inplace = True)
-        st.session_state.df = df_tot
+    if len(st.session_state['region_tiles']) > 0:
+        # Check dates
+        print(type(st.session_state['from_date']))
+        delta = st.session_state['to_date'] - st.session_state['from_date']
+        # print(delta.days)
+        if delta.days >= 0:
+            st.session_state['day_delta'] = 'Valid'
+            for req in request_list:
+                # print(req)
+                json = requests.get(req).json()
+                # print(json)
+                # DataFrame drlla risposta
+                if (len(json['value'])>0):
+                    df = pd.DataFrame.from_dict(json['value']).dropna()
+                    df = df[['Id', 'Name','OriginDate','GeoFootprint','Online']]
+                    # print('Immagini trovate per la singola richiesta: {}'.format(str(len(df))))
+                    df_list.append(df)
+                else:
+                    pass
+        
+            df_tot = pd.concat(df_list, ignore_index=True)
+            df_tot.sort_values(by='OriginDate', ascending = False, inplace = True)
+            st.session_state.df = df_tot
+        else:
+            st.session_state['day_delta'] = 'Invalid'
     else:
-        st.session_state['day_delta'] = 'Invalid'
+        pass
         
 st.markdown(
     """
@@ -71,46 +79,169 @@ with st.sidebar:
     st.subheader("Parametri di ricerca")
     
     regioni_italiane = [
-    'Abruzzo','Sardegna','Puglia','Bolzano','Calabria','Campania','Emilia_Romagna',
-    'Friuli', 'Lazio','Lombardia','Umbria_Marche','Piemonte','Sicilia','Toscana','Veneto']
-
-    regioni_italiane_ordinate = sorted(regioni_italiane)
+    'Abruzzo','Sardegna','Puglia','Bolzano','Calabria','Campania','Emilia Romagna',
+    'Friuli', 'Lazio','Lombardia','Umbria e Marche','Piemonte','Sicilia','Toscana','Veneto']
     
-    regione = st.selectbox('Regione', regioni_italiane_ordinate)
+    regione = st.multiselect('Regioni', sorted(regioni_italiane), max_selections=3, default=['Abruzzo'],
+                placeholder="Scegli una Regione")
     
-    # Tiles da scaricare in base alla regione selezionata
-    if regione == 'Abruzzo':
-        region_tiles = ['R122_T33TUG', 'R122_T33TUH', 'R079_T33TVG', 'R122_T33TVH']
-    elif regione == 'Sardegna':
-        region_tiles = ['R022_T32SMJ', 'R022_T32SNJ', 'R022_T32TMK', 'R065_T32TML',	'R022_T32TNK', 'R022_T32TNL']
-    elif regione == 'Puglia':
-        region_tiles = ['R079_T33TWF', 'R079_T33TWG', 'R036_T33TXE', 'R036_T33TXF', 'R036_T33TYE', 'R036_T33TYF']
-    elif regione == 'Bolzano':
-        region_tiles = ['R022_T32TPS', 'R065_T32TPT', 'R022_T32TQT']
-    elif regione == 'Calabria':
-        region_tiles = ['R036_T33SWC', 'R079_T33SWD', 'R036_T33SXC', 'R036_T33SXD']
-    elif regione == 'Campania':
-        region_tiles = ['R079_T33TVE', 'R079_T33TVF', 'R079_T33TWE']
-    elif regione == 'Emilia_Romagna':
-        region_tiles = ['R065_T32TNQ', 'R022_T32TPQ', 'R022_T32TQP', 'R022_T32TQQ']
-    elif regione == 'Friuli':
-        region_tiles =  ['R022_T33TUM', 'R122_T33TVL']
-    elif regione == 'Lazio':
-        region_tiles = ['R022_T32TPM', 'R122_T33TTF', 'R122_T33TTG', 'R122_T33TUF']
-    elif regione == 'Lombardia':
-        region_tiles = ['R065_T32TMR', 'R065_T32TMS', 'R065_T32TNR', 'R065_T32TNS']
-    elif regione == 'Umbria_Marche':
-        region_tiles = ['R122_T33TUJ', 'R122_T32TQN']
-    elif regione == 'Piemonte':
-        region_tiles = ['R108_T32TLP', 'R108_T32TLQ', 'R108_T32TLR', 'R108_T32TLS', 'R065_T32TMP', 'R065_T32TMQ']
-    elif regione == 'Sicilia':
-        region_tiles = ['R122_T33STV',	'R122_T33STA', 'R122_T33STB', 'R122_T33STC', 'R079_T33SUB', 'R079_T33SUC', 'R079_T33SUV', 'R079_T33SVA', 'R079_T33SVB',	'R079_T33SVC', 'R036_T33SWA', 'R036_T33SWB']
-    elif regione == 'Toscana':
-        region_tiles = ['R022_T32TNM',	'R022_T32TNN', 'R065_T32TNP', 'R022_T32TPN', 'R022_T32TPP']
-    elif regione == 'Veneto':
-        region_tiles = ['R022_T32TPR', 'R022_T32TQR', 'R022_T32TQS', 'R122_T33TUK', 'R122_T33TUL']
+    region_tiles = st.session_state['region_tiles']
     
-    tiles_to_search = st.multiselect("Tiles", region_tiles, default=region_tiles, placeholder="Scegli una Regione per popolare...")
+    if regione == []:
+        st.session_state['region_tiles'] = []              
+        
+    # Abruzzo
+    if 'Abruzzo' in regione:
+        for tile in ['R122_T33TUG', 'R122_T33TUH', 'R079_T33TVG', 'R122_T33TVH']:
+            if tile not in region_tiles:
+                region_tiles.append(tile)
+    else:
+        for tile in ['R122_T33TUG', 'R122_T33TUH', 'R079_T33TVG', 'R122_T33TVH']:
+            if tile in region_tiles:
+                region_tiles.remove(tile)
+    
+    # Sardegna
+    if 'Sardegna' in regione:
+        for tile in ['R022_T32SMJ', 'R022_T32SNJ', 'R022_T32TMK', 'R065_T32TML', 'R022_T32TNK', 'R022_T32TNL']:
+            if tile not in region_tiles:
+                region_tiles.append(tile)
+    else:
+        for tile in ['R022_T32SMJ', 'R022_T32SNJ', 'R022_T32TMK', 'R065_T32TML', 'R022_T32TNK', 'R022_T32TNL']:
+            if tile in region_tiles:
+                region_tiles.remove(tile)
+    
+    # Puglia
+    if 'Puglia' in regione:
+        for tile in ['R079_T33TWF', 'R079_T33TWG', 'R036_T33TXE', 'R036_T33TXF', 'R036_T33TYE', 'R036_T33TYF']:
+            if tile not in region_tiles:
+                region_tiles.append(tile)
+    else:
+        for tile in ['R079_T33TWF', 'R079_T33TWG', 'R036_T33TXE', 'R036_T33TXF', 'R036_T33TYE', 'R036_T33TYF']:
+            if tile in region_tiles:
+                region_tiles.remove(tile)
+    
+    # Bolzano
+    if 'Bolzano' in regione:
+        for tile in ['R022_T32TPS', 'R065_T32TPT', 'R022_T32TQT']:
+            if tile not in region_tiles:
+                region_tiles.append(tile)
+    else:
+        for tile in ['R022_T32TPS', 'R065_T32TPT', 'R022_T32TQT']:
+            if tile in region_tiles:
+                region_tiles.remove(tile)
+                
+    # Calabria
+    if 'Calabria' in regione:
+        for tile in ['R036_T33SWC', 'R079_T33SWD', 'R036_T33SXC', 'R036_T33SXD']:
+            if tile not in region_tiles:
+                region_tiles.append(tile)
+    else:
+        for tile in ['R036_T33SWC', 'R079_T33SWD', 'R036_T33SXC', 'R036_T33SXD']:
+            if tile in region_tiles:
+                region_tiles.remove(tile)
+    # Campania
+    if 'Campania' in regione:
+        for tile in ['R079_T33TVE', 'R079_T33TVF', 'R079_T33TWE']:
+            if tile not in region_tiles:
+                region_tiles.append(tile)
+    else:
+        for tile in ['R079_T33TVE', 'R079_T33TVF', 'R079_T33TWE']:
+            if tile in region_tiles:
+                region_tiles.remove(tile)
+    # Emilia
+    if 'Emilia Romagna' in regione:
+        for tile in ['R065_T32TNQ', 'R022_T32TPQ', 'R022_T32TQP', 'R022_T32TQQ']:
+            if tile not in region_tiles:
+                region_tiles.append(tile)
+    else:
+        for tile in ['R065_T32TNQ', 'R022_T32TPQ', 'R022_T32TQP', 'R022_T32TQQ']:
+            if tile in region_tiles:
+                region_tiles.remove(tile)
+    # Friuli
+    if 'Friuli' in regione:
+        for tile in ['R022_T33TUM', 'R122_T33TVL']:
+            if tile not in region_tiles:
+                region_tiles.append(tile)
+    else:
+        for tile in ['R022_T33TUM', 'R122_T33TVL']:
+            if tile in region_tiles:
+                region_tiles.remove(tile)
+    
+    # Lazio
+    if 'Lazio' in regione:
+        for tile in ['R022_T32TPM', 'R122_T33TTF', 'R122_T33TTG', 'R122_T33TUF']:
+            if tile not in region_tiles:
+                region_tiles.append(tile)
+    else:
+        for tile in ['R022_T32TPM', 'R122_T33TTF', 'R122_T33TTG', 'R122_T33TUF']:
+            if tile in region_tiles:
+                region_tiles.remove(tile)
+                
+    # Lombardia
+    if 'Lombardia' in regione:
+        for tile in ['R065_T32TMR', 'R065_T32TMS', 'R065_T32TNR', 'R065_T32TNS']:
+            if tile not in region_tiles:
+                region_tiles.append(tile)
+    else:
+        for tile in ['R065_T32TMR', 'R065_T32TMS', 'R065_T32TNR', 'R065_T32TNS']:
+            if tile in region_tiles:
+                region_tiles.remove(tile)
+    
+    # Umbria e Marche
+    if 'Umbria e Marche' in regione:
+        for tile in ['R122_T33TUJ', 'R122_T32TQN']:
+            if tile not in region_tiles:
+                region_tiles.append(tile)
+    else:
+        for tile in ['R122_T33TUJ', 'R122_T32TQN']:
+            if tile in region_tiles:
+                region_tiles.remove(tile)
+    
+    # Piemonte
+    if 'Piemonte' in regione:
+        for tile in ['R108_T32TLP', 'R108_T32TLQ', 'R108_T32TLR', 'R108_T32TLS', 'R065_T32TMP', 'R065_T32TMQ']:
+            if tile not in region_tiles:
+                region_tiles.append(tile)
+    else:
+        for tile in ['R108_T32TLP', 'R108_T32TLQ', 'R108_T32TLR', 'R108_T32TLS', 'R065_T32TMP', 'R065_T32TMQ']:
+            if tile in region_tiles:
+                region_tiles.remove(tile)
+    
+    # Sicilia
+    if 'Sicilia' in regione:
+        for tile in ['R122_T33STV',	'R122_T33STA', 'R122_T33STB', 'R122_T33STC', 'R079_T33SUB', 'R079_T33SUC', 'R079_T33SUV', 'R079_T33SVA', 'R079_T33SVB',	'R079_T33SVC', 'R036_T33SWA', 'R036_T33SWB']:
+            if tile not in region_tiles:
+                region_tiles.append(tile)
+    else:
+        for tile in ['R122_T33STV',	'R122_T33STA', 'R122_T33STB', 'R122_T33STC', 'R079_T33SUB', 'R079_T33SUC', 'R079_T33SUV', 'R079_T33SVA', 'R079_T33SVB',	'R079_T33SVC', 'R036_T33SWA', 'R036_T33SWB']:
+            if tile in region_tiles:
+                region_tiles.remove(tile)
+    
+    # Toscana      
+    if 'Toscana' in regione:
+        for tile in ['R022_T32TNM',	'R022_T32TNN', 'R065_T32TNP', 'R022_T32TPN', 'R022_T32TPP']:
+            if tile not in region_tiles:
+                region_tiles.append(tile)
+    else:
+        for tile in ['R022_T32TNM',	'R022_T32TNN', 'R065_T32TNP', 'R022_T32TPN', 'R022_T32TPP']:
+            if tile in region_tiles:
+                region_tiles.remove(tile)
+                
+    # Veneto
+    if 'Veneto' in regione:
+        for tile in ['R022_T32TPR', 'R022_T32TQR', 'R022_T32TQS', 'R122_T33TUK', 'R122_T33TUL']:
+            if tile not in region_tiles:
+                region_tiles.append(tile)
+    else:
+        for tile in ['R022_T32TPR', 'R022_T32TQR', 'R022_T32TQS', 'R122_T33TUK', 'R122_T33TUL']:
+            if tile in region_tiles:
+                region_tiles.remove(tile)
+            
+    
+    tiles_to_search = st.multiselect("Tiles", region_tiles, default=st.session_state['region_tiles'],
+                            placeholder="Scegli una Regione per popolare...")
+    
+    print(st.session_state['region_tiles'])
     
     sideCol1, sideCol2 = st.columns(2)
     with sideCol1:
@@ -136,7 +267,7 @@ with st.sidebar:
     request_list = []
     
     if len(tiles_to_search) == 0:
-        tiles_to_search = region_tiles
+        tiles_to_search = st.session_state['region_tiles']
     
     for tile in tiles_to_search:
         filter_tile = "contains(Name, '_{}_')".format(tile)
